48ba0a84d5ce15266090f9857931e43968f66c86
IdaRu <tuulikki.93@hotmail.com>
2018-09-03 21:43:57 +0300

    Assignment 1

---
+++ Webappi/ICityBikeDataFetcher.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Webappi
+{
+    public interface ICityBikeDataFetcher
+    {
+        Task<int> GetBikeCountInStation(string stationName);
+    }
+}
